c413b44bb1bd484181a9ebe51f9fe0909404aa2d
Ranjitha-Duraisamy27 <ranjitha.duraisamy27@gmail.com>
2022-08-05 20:36:19 +0530

    added navbar

--- index.js
+++ index.js
@@ -1,28 +1,85 @@
 function Navbar() {
-    return(
-        <nav className="navbar navbar-dark bg-dark">
-            <header className="container">
-                <a href="#" className="navbar-brand">Header</a>
-            </header>
-        </nav>
-    )
+  return (
+    <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          ReactJS
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown link
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 function MainContent() {
-    return (
-        <main className="container py-3">
-            <article>
-                Main Content
-            </article>
-        </main>
-    );
+  return (
+    <main className="container py-3">
+      <article>Learning React</article>
+    </main>
+  );
 }
 
-
 ReactDOM.render(
-    <div>
-        <Navbar />
-        <MainContent />
-    </div>,
-    document.getElementById('root')
+  <div>
+    <Navbar />
+    <MainContent />
+  </div>,
+  document.getElementById("root")
 );
